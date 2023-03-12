e566f7700b5a8620c2fb0c311cbb7f85424472b9
LucasMatheusDev <lucasmatheusdev@gmail.com>
2023-03-12 09:35:21 -0300

    add body in html

--- send_email.py
+++ send_email.py
@@ -10,19 +10,19 @@ def send_email(open_box_products: list[OpenBoxProduct], send_email_info: SendEma
     number_of_errors = 0
     try:
         # # Converte a lista de produtos para uma string
-        products_str = ''
+        products_html = ''
         for open_box_product in open_box_products:
-            products_str += f"Produto: {open_box_product.name}\n Preços: {open_box_product.prices.replace(',', '')}\n Link: {open_box_product.link}\n\n"
-        print(f'produtos encontrados: {products_str}')
+            products_html += f'<p><b>Produto:</b> {open_box_product.name}<br><b>Preços:</b> {open_box_product.prices.replace(",", "")}<br><b>Link:</b> <a href="{open_box_product.link}">{open_box_product.link}</a></p>'
+        print(f'produtos encontrados: {products_html}')
 
         # Cria a mensagem a ser enviada
-        body_message = f"Lista de produtos Open Box disponiveis:\n{products_str}"
+        body_message = f'<html><body><p>Lista de produtos Open Box disponiveis:</p>{products_html}</body></html>'
         message = MIMEMultipart()
 
         message["From"] = send_email_info.email_from
         message["To"] = send_email_info.email_to
         message["Subject"] = "Lista de Produtos Open Box"
-        message.attach(MIMEText(body_message, "plain"))
+        message.attach(MIMEText(body_message, "html"))
 
         with smtplib.SMTP(send_email_info.smtp_server, port=send_email_info.smtp_port) as connection:
             connection.starttls()
